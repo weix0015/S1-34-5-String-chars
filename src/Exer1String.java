a502c704e8239d79851e55aac3d936028abee564
weix0015 <weix0015@stud.kea.dk>
2022-10-13 14:51:54 +0200

    S1-34-5-String-chars

---
+++ src/Exer1String.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Exer1String{
+    public static void main(String[] args){
+        Scanner b = new Scanner(System.in);
+        String a = b.next();
+        System.out.println(a);
+    }
+}
